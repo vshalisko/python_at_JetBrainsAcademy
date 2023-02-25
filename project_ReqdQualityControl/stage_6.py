7e5a68a6ac723890342ea031b3fa49dcedca5811
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2023-02-24 20:03:41 -0600

    Update stage_6.py

--- project_ReqdQualityControl/stage_6.py
+++ project_ReqdQualityControl/stage_6.py
@@ -1,23 +1,14 @@
 
 import string
 import gzip
-#import os
-#import io
-#import math
-
-#print(os.path())
-
-#fastq_file = str(input())
-#fastq = open(fastq_file, "r")
-#data = fastq.readlines()
-#fastq.close()
 
 def read_gzip(file_name):
-    print(file_name)
-    with gzip.open(file1, 'rb') as f:
-        print(f)
+    #print(file_name)
+    with gzip.open(file1, 'rt') as f:
+        #print(f)
+        #file_content = f.read().splitlines()
         file_content = f.read().splitlines()
-        print(file_content)
+        #print(file_content)
     return file_content
 
 def read_fastq(file_name):
@@ -30,9 +21,9 @@ def quality_scores(data):
     lengths = dict()
     reads_dict = dict()
     reads_count = 0
-    reads_with_ns_count = 0
+    n_reads = 0
     length_sum = 0
-    repits_sum = 0
+    repeats_sum = 0
     proportion_cg_sum = 0
     proportion_ns_sum = 0
     for i in range(0, len(data)):
@@ -43,8 +34,8 @@ def quality_scores(data):
             count_c = stripped_sequence.upper().count('C')
             count_g = stripped_sequence.upper().count('G')
             count_n = stripped_sequence.upper().count('N')
-
             read_length = len(stripped_sequence)
+
             proportion_cg = 100 * (count_c + count_g) / read_length
             length_sum += read_length
             proportion_cg_sum += proportion_cg
@@ -52,7 +43,7 @@ def quality_scores(data):
             proportion_ns_sum += proportion_ns
 
             if count_n != None and count_n > 0:
-                reads_with_ns_count += 1
+                n_reads += 1
 
             if lengths.get(read_length) == None:
                 lengths[read_length] = 1
@@ -63,38 +54,41 @@ def quality_scores(data):
                 reads_dict[stripped_sequence] = 1
             else:
                 reads_dict[stripped_sequence] += 1
-                repits_sum += 1
+                repeats_sum += 1
 
     length_average = int(round(length_sum / reads_count, 0))
     proportion_cg_average = round(proportion_cg_sum / reads_count, 2)
     proportion_ns_average = round(proportion_ns_sum / reads_count, 2)
-    return reads_count, length_average, repits_sum, reads_with_ns_count, proportion_cg_average, proportion_ns_average
-
+    files_data.append({
+        'reads_count': reads_count,
+        'length_average': length_average,
+        'repeats_sum': repeats_sum,
+        'n_reads': n_reads,
+        'proportion_cg_average': proportion_cg_average,
+        'proportion_ns_average': proportion_ns_average
+    })
 
 ## input reading
 file1 = str(input())
 file2 = str(input())
 file3 = str(input())
 
-## gunzipping - FAILURE sithe first test data
-#a, b, c, d, e, f = quality_scores(read_fastq(file1))
-a1, b1, c1, d1, e1, f1 = quality_scores(read_gzip(file1))
-a2, b2, c2, d2, e2, f2 = quality_scores(read_gzip(file2))
-a3, b3, c3, d3, e3, f3 = quality_scores(read_gzip(file3))
+files_data = []
 
+## Gunzipping and quality scores
+quality_scores(read_gzip(file1))
+quality_scores(read_gzip(file2))
+quality_scores(read_gzip(file3))
 
-## bert score selector
-index1 = f1 + c1
-index2 = f2 + c2
-index3 = f3 + c3
-
-a, b, c, d, e, f = a1, b1, c1, d1, e1, f1
+best = files_data[0]
+for data in files_data:
+    if data['repeats_sum'] < best['repeats_sum'] and data['n_reads'] < best['n_reads']:
+        best = data
 
 ## output
-print("Reads in the file = {}:".format(a))
-print("Reads sequence average length = {}".format(b))
-print("\nRepeats = {}".format(c))
-print("Reads with Ns = {}".format(d))
-print("\nGC content average = {}%".format(e))
-print("Ns per read sequence = {}%".format(f))
-
+print("Reads in the file = {}:".format(best["reads_count"]))
+print("Reads sequence average length = {}".format(best["length_average"]))
+print("\nRepeats = {}".format(best["repeats_sum"]))
+print("Reads with Ns = {}".format(best["n_reads"]))
+print("\nGC content average = {}%".format(best["proportion_cg_average"]))
+print("Ns per read sequence = {}%".format(best["proportion_ns_average"]))
