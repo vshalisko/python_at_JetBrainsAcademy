8b6810d5c2dba3c9468df78a87ab9341ed2b9555
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2023-09-27 19:50:09 -0600

    Update stage_1.py

--- Project_Classification_of_Handwritten_Digits/stage_1.py
+++ Project_Classification_of_Handwritten_Digits/stage_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 
 #train_ds = tf.keras.datasets.mnist.load_data(path="mnist.npz")
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+(x_train, y_train), (_, _) = tf.keras.datasets.mnist.load_data()
 
 #x_train = x_train.reshape(60000, 784).astype('float32') / 255
 x_train = x_train.reshape(60000, 784)
