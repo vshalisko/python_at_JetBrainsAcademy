e8ed3f0f170d12119997f29eef40e2c1bad1fb15
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2023-09-27 19:46:42 -0600

    Create stage_1.py

---
+++ Project_Classification_of_Handwritten_Digits/stage_1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+
+#train_ds = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+#x_train = x_train.reshape(60000, 784).astype('float32') / 255
+x_train = x_train.reshape(60000, 784)
+
+#print(len(x_train))
+print("Classes: {}".format(np.unique(y_train)))
+print("Features' shape: {}".format(np.shape(x_train)))
+print("Target's shape: {}".format(np.shape(y_train)))
+print("min: {}, max: {}".format(np.min(x_train),np.max(x_train)))
