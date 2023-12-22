5ce549d0b0d1d9276b6b74ed23f7e2284bedf05e
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2023-12-21 20:24:23 -0600

    Update Stage_1.py

--- Project_LinearRegressionFromScratch/Stage_1.py
+++ Project_LinearRegressionFromScratch/Stage_1.py
@@ -1,9 +1,9 @@
+# write your code here
 import numpy as np
 
 class CustomLinearRegression:
 
     def __init__(self, *, fit_intercept=True):
-
         self.fit_intercept = fit_intercept
         self.coefficient = []
         self.intercept = []
@@ -15,12 +15,6 @@ class CustomLinearRegression:
         solution = np.dot(np.linalg.inv(b), a)
         self.intercept = solution[-1]
         self.coefficient = solution[0:-1]
-        if (self.fit_intercept == True):
-            result = {'Intercept': self.intercept,
-                      'Coefficient': self.coefficient}
-        else:
-            result = {'Coefficient': self.coefficient}
-        return(result)
 
 x = [4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0]
 y = [33, 42, 45, 51, 53, 61, 62]
@@ -29,4 +23,11 @@ X = np.array([x,np.ones(7)]).T
 y = np.array(y)
 
 lr = CustomLinearRegression(fit_intercept=True)
-print(lr.fit(X, y))
+lr.fit(X, y)
+
+if (lr.fit_intercept == True):
+    result = {'Intercept': lr.intercept,
+              'Coefficient': lr.coefficient}
+else:
+    result = {'Coefficient': lr.coefficient}
+print(result)
