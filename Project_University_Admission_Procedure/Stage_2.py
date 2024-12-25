babd7971a8c4a82bc29cae084d580ed5e644aab6
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2024-12-25 13:30:21 -0600

    Create Stage_2.py

---
+++ Project_University_Admission_Procedure/Stage_2.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+score = (a + b + c) / 3
+print(score)
+if (score >= 60):
+    print("Congratulations, you are accepted!")
+else:
+    print("We regret to inform you that we will not be able to offer you admission.")
