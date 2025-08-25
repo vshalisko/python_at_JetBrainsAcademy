7e4276b5d91b13144f8af625db06cfbf8a97319f
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-08-24 21:47:24 -0600

    Create Stage_1.py

---
+++ Project_Jeopardy_Question_Answering_Bot/Stage_1.py
@@ -0,0 +1,9 @@
+bot_name = 'Charles Kew'
+
+print(f"Hello! I'm {bot_name}, a question answering bot who knows answers to all questions from the 'Jeopardy!' game.")
+
+while True:
+    print('Ask me something!')
+    question = input()
+    print("Let's play!")
+    break
