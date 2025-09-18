9600965af0541de07e4be7d2da5df7a650dec772
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-09-17 23:06:25 -0600

    Update Stage_4.py
    The solution that passed tests

--- Project_Readability_Score/Stage_4.py
+++ Project_Readability_Score/Stage_4.py
@@ -21,8 +21,6 @@ age_scores = {
     '14': '18-22'
 }
 
-vowels = ("a", "e", "i", "o", "u", "y")
-
 filename = sys.argv[1]
 
 text = ''
@@ -36,53 +34,57 @@ sentences = nltk.sent_tokenize(text)
 words = nltk.word_tokenize(text)
 
 sentence_count = len(sentences)
-
 word_count = 0
 for sentence in sentences:
     sentence_words = tokenizer.tokenize(sentence)
     word_count += len(sentence_words)
 
-def syllables(word):
-    ## from https://medium.com/@mholtzscher/programmatically-counting-syllables-ca760435fab4
-    syllable_count = 0
-    vowels = 'aeiouy'
-    if word[0] in vowels:
-        syllable_count += 1
-    for index in range(1, len(word)):
-        if word[index] in vowels and word[index - 1] not in vowels:
-            syllable_count += 1
-    if word.endswith('e'):
-        syllable_count -= 1
-    if word.endswith('le') and len(word) > 2 and word[-3] not in vowels:
-        syllable_count += 1
-    if syllable_count == 0:
-        syllable_count += 1
-    return syllable_count
-
-def count_syllables(text):
-    word = text.lower()
-    pattern = r"[aeiouy]{2}|[^aiouy][aiouy][^aiouy]|e\B"
-    #pattern = r"([aeiouyAEIOUY]+[^e.\s])|([aiouyAEIOUY]+\b)|(\b[^aeiouy0-9.']+e\b)"
-    x = re.findall(pattern, word)
-    return len(x) or 1
-
 symbol_count = 0
-syllable_count = 0
 for word in words:
     symbol_count += len(word)
-    syllable_count += count_syllables(word)
+
+def countSyllables(text):
+    ## The following function was taken from Joydeep Chatterjee solution
+    syllables = 0
+    words = text.lower().split(" ")
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    for word in words:
+        count = 0
+        i = 0
+        j = 1
+        while j < len(word):
+            if word[i] in vowels:
+                if word[j] not in vowels:
+                    if j - i > 2:
+                        count += 2
+                    else:
+                        count += 1
+                    i = j
+                    j = i + 1
+                else: j += 1
+            else:
+                i += 1
+                j += 1
+        if count == 0:
+            count += 1
+        syllables += count
+    return syllables
+
+syllable_count = countSyllables(text)
 
 ar_score = math.ceil(4.71 * symbol_count / word_count + 0.5 * word_count / sentence_count - 21.43)
 fk_score = math.ceil(0.39 * word_count / sentence_count + 11.8 * syllable_count / word_count - 15.59)
 
 low_age0, top_age0 = age_scores[str(ar_score)].split("-")
 low_age1, top_age1 = age_scores[str(fk_score)].split("-")
-average_age = (int(low_age0) + int(low_age1) + int(top_age0) + int(top_age1)) / 4
+average_age0 = (int(low_age0) + int(top_age0)) / 2
+average_age1 = (int(low_age1) + int(top_age1)) / 2
+average_age = (average_age0 + average_age1) / 2
 
 print(f"Characters: {symbol_count}")
 print(f"Sentences: {sentence_count}")
 print(f"Words: {word_count}")
 print(f"Syllables: {syllable_count}")
 print(f"\nAutomated Readability Index: {ar_score} (about {age_scores[str(ar_score)]} year olds).")
-print(f"Flesch–Kincaid Readability Test: {ar_score} (about {age_scores[str(ar_score)]} year olds).")
+print(f"Flesch–Kincaid Readability Test: {fk_score} (about {age_scores[str(fk_score)]} year olds).")
 print(f"\nThis text should be understood in average by {average_age} year olds.")
