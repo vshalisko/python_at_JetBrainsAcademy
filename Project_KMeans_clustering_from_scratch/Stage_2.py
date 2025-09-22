a1650a59be4b832405d67a2f3293bc17280d1aef
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-09-22 14:21:58 -0600

    Create Stage_2.py

---
+++ Project_KMeans_clustering_from_scratch/Stage_2.py
@@ -0,0 +1,104 @@
+from cgi import print_form
+
+import numpy as np
+from sklearn.datasets import load_wine
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+
+def plot_comparison(data: np.ndarray, predicted_clusters: np.ndarray, true_clusters: np.ndarray = None,
+                    centers: np.ndarray = None, show: bool = True):
+
+    # Use this function to visualize the results on Stage 6.
+
+    if true_clusters is not None:
+        plt.figure(figsize=(20, 10))
+
+        plt.subplot(1, 2, 1)
+        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=predicted_clusters, palette='deep')
+        if centers is not None:
+            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
+        plt.title('Predicted clusters')
+        plt.xlabel('alcohol')
+        plt.ylabel('malic_acid')
+        plt.grid()
+
+        plt.subplot(1, 2, 2)
+        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=true_clusters, palette='deep')
+        if centers is not None:
+            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
+        plt.title('Ground truth')
+        plt.xlabel('alcohol')
+        plt.ylabel('malic_acid')
+        plt.grid()
+    else:
+        plt.figure(figsize=(10, 10))
+        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=predicted_clusters, palette='deep')
+        if centers is not None:
+            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
+        plt.title('Predicted clusters')
+        plt.xlabel('alcohol')
+        plt.ylabel('malic_acid')
+        plt.grid()
+
+    plt.savefig('Visualization.png', bbox_inches='tight')
+    if show:
+        plt.show()
+
+
+def euclidean(a, b):
+    ## both a & b atr numpy arrays
+    squared_difference = np.square(a - b)
+    return np.sqrt(np.sum(squared_difference))
+
+def find_nearest_center(X, centers):
+    distances = np.zeros((X.shape[0], centers.shape[0]))
+    for i in range(0, X.shape[0]):
+        for j in range(0, centers.shape[0]):
+            distances[i,j] = euclidean(X[i,],X[j,])
+    return np.argmin(distances, axis=1)
+
+def calculate_new_centers(X, labels):
+    levels = np.unique(labels)
+    #print(levels)
+    new_centers = np.zeros((len(levels), X.shape[1]))
+    for level in levels:
+        selector = labels == level
+        #print(selector)
+        means = np.mean(X[selector,:], axis=0)
+        #print(means)
+        new_centers[level,:] = means
+
+    #print(new_centers)
+    return new_centers
+
+if __name__ == '__main__':
+
+    # Load data
+    data = load_wine(as_frame=True, return_X_y=True)
+    X_full, y_full = data
+
+    # Permutate it to make things more interesting
+    rnd = np.random.RandomState(42)
+    permutations = rnd.permutation(len(X_full))
+    X_full = X_full.iloc[permutations]
+    y_full = y_full.iloc[permutations]
+
+    # From dataframe to ndarray
+    X_full = X_full.values
+    y_full = y_full.values
+
+    # Scale data
+    scaler = StandardScaler()
+    X_full = scaler.fit_transform(X_full)
+
+    centers_start = X_full[0:3,]
+
+    y = find_nearest_center(X_full, centers_start)
+    #cs = ', '.join(map(str, y[-10:]))
+    #print(f"[{cs}]")
+
+    centers_new = calculate_new_centers(X_full, y)
+    centers_flatten = centers_new.flatten()
+    cs = ', '.join(map(str, centers_flatten))
+    print(f"[{cs}]")
