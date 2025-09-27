ca756388ccc0de33f6dcb7ad05f219e0db1d7e06
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-09-26 18:05:15 -0600

    Update Stage_3.py
    Removed unncessary code

--- Project_KMeans_clustering_from_scratch/Stage_3.py
+++ Project_KMeans_clustering_from_scratch/Stage_3.py
@@ -1,49 +1,6 @@
-from cgi import print_form
-
 import numpy as np
 from sklearn.datasets import load_wine
-from matplotlib import pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import StandardScaler
-
-def plot_comparison(data: np.ndarray, predicted_clusters: np.ndarray, true_clusters: np.ndarray = None,
-                    centers: np.ndarray = None, show: bool = True):
-
-    # Use this function to visualize the results on Stage 6.
-
-    if true_clusters is not None:
-        plt.figure(figsize=(20, 10))
-
-        plt.subplot(1, 2, 1)
-        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=predicted_clusters, palette='deep')
-        if centers is not None:
-            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
-        plt.title('Predicted clusters')
-        plt.xlabel('alcohol')
-        plt.ylabel('malic_acid')
-        plt.grid()
-
-        plt.subplot(1, 2, 2)
-        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=true_clusters, palette='deep')
-        if centers is not None:
-            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
-        plt.title('Ground truth')
-        plt.xlabel('alcohol')
-        plt.ylabel('malic_acid')
-        plt.grid()
-    else:
-        plt.figure(figsize=(10, 10))
-        sns.scatterplot(x=data[:, 0], y=data[:, 1], hue=predicted_clusters, palette='deep')
-        if centers is not None:
-            sns.scatterplot(x=centers[:, 0], y=centers[:, 1], marker='X', color='k', s=200)
-        plt.title('Predicted clusters')
-        plt.xlabel('alcohol')
-        plt.ylabel('malic_acid')
-        plt.grid()
-
-    plt.savefig('Visualization.png', bbox_inches='tight')
-    if show:
-        plt.show()
 
 class CustomKMeans:
     def __init__(self, k):
