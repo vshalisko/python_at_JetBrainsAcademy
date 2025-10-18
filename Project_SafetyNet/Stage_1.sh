08983440b54d50d3a566a1b9f56c0f0a43db3cb0
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-10-18 17:24:44 -0600

    Create Stage_1.sh

---
+++ Project_SafetyNet/Stage_1.sh
@@ -0,0 +1,6 @@
+# You need to install InelliJ IDE, the Jet Brains Academy plugin and to start the project in IDE
+# The JDK 23 is required as well
+git clone https://github.com/hyperskill-content/Safety-net-study-repository.git
+cd .\Safety-net-study-repository\
+git checkout 0.2.x-dev
+git branch --all
