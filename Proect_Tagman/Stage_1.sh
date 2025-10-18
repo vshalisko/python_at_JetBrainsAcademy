bd80f1d44f0a136c2d7037bf6d2fa1658a1b369f
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-10-18 15:33:32 -0600

    Create Stage_1.sh

---
+++ Proect_Tagman/Stage_1.sh
@@ -0,0 +1,5 @@
+# In the current working directory, initialize an empty Git repository;
+# Add a remote to your repository, with the name origin and with the URL file:///tmp/tagger.
+
+git init
+git remote add origin file:///tmp/tagger
