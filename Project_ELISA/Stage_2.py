0e27c927e2e7056946bdc4efaaec3b7a7743da78
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2025-10-29 18:17:58 -0600

    Create Stage_2.py

---
+++ Project_ELISA/Stage_2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from scipy.optimize import curve_fit
+
+def func(x, A, B, C, D):
+    return D + ((A - D) / (1 + (x / C) ** B))
+
+stage = 2
+
+if stage == 1:
+    OD_file_name = input()
+    OD_file = pd.read_csv(OD_file_name)
+
+    OD_file['AverageOD'] = (OD_file['OD1'] + OD_file['OD2']) / 2
+    blank_average = OD_file.loc[OD_file['Sample'] == 'BLANK','AverageOD']
+
+    OD_file['CorrectedOD'] = OD_file['AverageOD'] - blank_average.item()
+
+    OD_file.to_csv('elisa_result.csv', index=False)
+
+
+if stage == 2:
+    ST_file_name = input()
+    ST_file = pd.read_csv(ST_file_name)
+
+    ST_file['AverageOD'] = (ST_file['OD1'] + ST_file['OD2']) / 2
+
+    popt, pcov = curve_fit(f=func,
+                           ydata=ST_file['AverageOD'],
+                           xdata=ST_file['Concentration (ng/ml)'])
+
+    answer = '{:.8f} {:.8f} {:.8f} {:.8f}'.format(popt[0], popt[1], popt[2], popt[3])
+    print(answer)
